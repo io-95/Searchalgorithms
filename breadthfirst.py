02ce7e1d7f717bf708db6685034b4bea703e21f3
io-95 <77495768+io-95@users.noreply.github.com>
2022-11-07 21:59:11 +0100

    set up breadthfirst.py and main calls breadthfirstsearch method

--- breadthfirst.py
+++ breadthfirst.py
@@ -1,2 +1,12 @@
 class Breadthfirst:
-    pass
+    @staticmethod
+    def breadthfirstsearch(nodelist, goal):
+        print(nodelist)
+
+        # newNodes = []
+        # for node in nodelist:
+        #    if node == goal:
+        #        return node
+
+    def goalreached(self):
+        pass
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import breadthfirst
+from breadthfirst import Breadthfirst
+
 
 def main():
     n = 3
-    a = [[None]*n]*n
+    a = [[None] * n] * n
     for i in range(n):
-        a[i] = input('please type in row {0}: ').format(i)
+        a[i] = input('please type in row {0}: ').format(i).split()
 
-    print(a)
+    goal = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '0']]
+    Breadthfirst.breadthfirstsearch(a, goal)
+
 
 if __name__ == '__main__':
     main()
