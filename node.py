29a9681477dc3e42f60df894b03bec898a7abe30
Ibrahim <ibrahim.oezel@hotmail.de>
2022-11-08 10:58:49 +0100

    create class Node

---
+++ node.py
@@ -0,0 +1,10 @@
+class Node:
+
+    predecessor = None
+    node = None
+
+    # Constructor
+    def __init__(self, node, predecessor)
+
+       self.node = node
+       self.predecessor = predecessor
