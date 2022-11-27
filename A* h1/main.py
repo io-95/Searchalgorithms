c2b8998244fecdaf688d5a87a8152e961a684918
io-95 <77495768+io-95@users.noreply.github.com>
2022-11-27 20:30:18 +0100

    delete nodelist in main, heuristic search just need the firstnode

--- A* h1/main.py
+++ A* h1/main.py
@@ -9,11 +9,9 @@ def main():
         a.append([int(j) for j in input('type in row {}(space after every digit please): '.format(i)).split()])
 
     firstnode = Node(a.copy(), None)
-    nodelist = []
-    nodelist.append(firstnode)
 
     goal = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
-    Breadthfirst.breadthfirstsearch(nodelist, goal)
+    A_star.heuristicSearch(firstnode, goal)
 
 
 if __name__ == '__main__':
