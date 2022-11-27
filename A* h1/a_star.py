9ba78adf459236cfad2f5e86c18e5396a20db98d
io-95 <77495768+io-95@users.noreply.github.com>
2022-11-27 21:27:01 +0100

    successor calculates the h values of the created nodes

--- A* h1/a_star.py
+++ A* h1/a_star.py
@@ -48,75 +48,98 @@ class A_star:
         if positionZero[0] == 0 and positionZero[1] == 0:
             newNode1 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode1.node[0][0], newNode1.node[0][1] = newNode1.node[0][1], newNode1.node[0][0]
+            Node.calc_h(newNode1)
             newNode2 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode2.node[0][0], newNode2.node[1][0] = newNode2.node[1][0], newNode2.node[0][0]
-
+            Node.calc_h(newNode2)
             return [newNode1, newNode2]
 
         if positionZero[0] == 0 and positionZero[1] == 1:
             newNode1 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode1.node[0][1], newNode1.node[0][0] = newNode1.node[0][0], newNode1.node[0][1]
+            Node.calc_h(newNode1)
             newNode2 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode2.node[0][1], newNode2.node[0][2] = newNode2.node[0][2], newNode2.node[0][1]
+            Node.calc_h(newNode2)
             newNode3 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode3.node[0][1], newNode3.node[1][1] = newNode3.node[1][1], newNode3.node[0][1]
+            Node.calc_h(newNode3)
             return [newNode1, newNode2, newNode3]
 
         if positionZero[0] == 0 and positionZero[1] == 2:
             newNode1 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode1.node[0][2], newNode1.node[0][1] = newNode1.node[0][1], newNode1.node[0][2]
+            Node.calc_h(newNode1)
             newNode2 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode2.node[0][2], newNode2.node[1][2] = newNode2.node[1][2], newNode2.node[0][2]
+            Node.calc_h(newNode2)
             return [newNode1, newNode2]
 
         if positionZero[0] == 1 and positionZero[1] == 0:
             newNode1 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode1.node[1][0], newNode1.node[0][0] = newNode1.node[0][0], newNode1.node[1][0]
+            Node.calc_h(newNode1)
             newNode2 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode2.node[1][0], newNode2.node[1][1] = newNode2.node[1][1], newNode2.node[1][0]
+            Node.calc_h(newNode2)
             newNode3 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode3.node[1][0], newNode3.node[2][0] = newNode3.node[2][0], newNode3.node[1][0]
+            Node.calc_h(newNode3)
             return [newNode1, newNode2, newNode3]
 
         if positionZero[0] == 1 and positionZero[1] == 1:
             newNode1 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode1.node[1][1], newNode1.node[0][1] = newNode1.node[0][1], newNode1.node[1][1]
+            Node.calc_h(newNode1)
             newNode2 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode2.node[1][1], newNode2.node[1][0] = newNode2.node[1][0], newNode2.node[1][1]
+            Node.calc_h(newNode2)
             newNode3 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode3.node[1][1], newNode3.node[1][2] = newNode3.node[1][2], newNode3.node[1][1]
+            Node.calc_h(newNode3)
             newNode4 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode4.node[1][1], newNode4.node[2][1] = newNode4.node[2][1], newNode4.node[1][1]
+            Node.calc_h(newNode4)
             return [newNode1, newNode2, newNode3, newNode4]
 
         if positionZero[0] == 1 and positionZero[1] == 2:
             newNode1 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode1.node[1][2], newNode1.node[0][2] = newNode1.node[0][2], newNode1.node[1][2]
+            Node.calc_h(newNode1)
             newNode2 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode2.node[1][2], newNode2.node[1][1] = newNode2.node[1][1], newNode2.node[1][2]
+            Node.calc_h(newNode2)
             newNode3 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode3.node[1][2], newNode3.node[2][2] = newNode3.node[2][2], newNode3.node[1][2]
+            Node.calc_h(newNode3)
             return [newNode1, newNode2, newNode3]
 
         if positionZero[0] == 2 and positionZero[1] == 0:
             newNode1 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode1.node[2][0], newNode1.node[1][0] = newNode1.node[1][0], newNode1.node[2][0]
+            Node.calc_h(newNode1)
             newNode2 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode2.node[2][0], newNode2.node[2][1] = newNode2.node[2][1], newNode2.node[2][0]
+            Node.calc_h(newNode2)
             return [newNode1, newNode2]
 
         if positionZero[0] == 2 and positionZero[1] == 1:
             newNode1 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode1.node[2][1], newNode1.node[2][0] = newNode1.node[2][0], newNode1.node[2][1]
+            Node.calc_h(newNode1)
             newNode2 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode2.node[2][1], newNode2.node[1][1] = newNode2.node[1][1], newNode2.node[2][1]
+            Node.calc_h(newNode2)
             newNode3 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode3.node[2][1], newNode3.node[2][2] = newNode3.node[2][2], newNode3.node[2][1]
+            Node.calc_h(newNode3)
             return [newNode1, newNode2, newNode3]
 
         if positionZero[0] == 2 and positionZero[1] == 2:
             newNode1 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode1.node[2][2], newNode1.node[2][1] = newNode1.node[2][1], newNode1.node[2][2]
+            Node.calc_h(newNode1)
             newNode2 = Node(copy.deepcopy(currentNode.node), currentNode)
             newNode2.node[2][2], newNode2.node[1][2] = newNode2.node[1][2], newNode2.node[2][2]
+            Node.calc_h(newNode2)
             return [newNode1, newNode2]
