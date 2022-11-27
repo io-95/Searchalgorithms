5e828aacd2c29fcf3d0b011db10a7a1036b9fee5
io-95 <77495768+io-95@users.noreply.github.com>
2022-11-27 19:48:04 +0100

    create folder structure of A start algorithm

---
+++ A* h1/main.py
@@ -0,0 +1,20 @@
+from a_star import A_star
+from node import Node
+
+def main():
+    n = 3
+    a = []
+
+    for i in range(n):
+        a.append([int(j) for j in input('type in row {}(space after every digit please): '.format(i)).split()])
+
+    firstnode = Node(a.copy(), None)
+    nodelist = []
+    nodelist.append(firstnode)
+
+    goal = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+    Breadthfirst.breadthfirstsearch(nodelist, goal)
+
+
+if __name__ == '__main__':
+    main()
