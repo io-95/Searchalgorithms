fd1dde64e6b4d36eeef428e316b6f1ef506ed71a
io-95 <77495768+io-95@users.noreply.github.com>
2022-12-03 21:41:00 +0100

    imlement cyclecheck

--- A* h1/a_star.py
+++ A* h1/a_star.py
@@ -20,7 +20,6 @@ class A_star:
                 return
             successor = A_star.successor(node)
             A_star.sortin(successor, nodelist)
-            # print(nodelist)
 
     @staticmethod
     def goalreached(aNode, goal):
@@ -32,7 +31,17 @@ class A_star:
     def sortin(successor, nodelist):
         for node in successor:
             bisect.insort(nodelist, node)
-            print(Node.calc_f(node))
+
+    @staticmethod
+    def cycleCheck(node):
+        if node.predecessor.predecessor is None:
+            return True
+
+        res = [x for x in node.node + node.predecessor.predecessor.node if x not in node.node or x not in node.predecessor.predecessor.node]
+        if len(res) == 0:
+            return False
+        else:
+            return True
 
     @staticmethod
     def successor(currentNode):
@@ -43,102 +52,176 @@ class A_star:
                 if currentNode.node[i][j] == 0:
                     positionZero.append(i)
                     positionZero.append(j)
+                    break
+
+        returnValue = []
 
         if positionZero[0] == 0 and positionZero[1] == 0:
             newNode1 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode1.node[0][0], newNode1.node[0][1] = newNode1.node[0][1], newNode1.node[0][0]
             Node.calc_h(newNode1)
+            if A_star.cycleCheck(newNode1):
+                returnValue.append(newNode1)
+
             newNode2 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode2.node[0][0], newNode2.node[1][0] = newNode2.node[1][0], newNode2.node[0][0]
             Node.calc_h(newNode2)
-            return [newNode1, newNode2]
+            if A_star.cycleCheck(newNode2):
+                returnValue.append(newNode2)
+
+            return returnValue
 
         if positionZero[0] == 0 and positionZero[1] == 1:
             newNode1 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode1.node[0][1], newNode1.node[0][0] = newNode1.node[0][0], newNode1.node[0][1]
             Node.calc_h(newNode1)
+            if A_star.cycleCheck(newNode1):
+                returnValue.append(newNode1)
+
             newNode2 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode2.node[0][1], newNode2.node[0][2] = newNode2.node[0][2], newNode2.node[0][1]
             Node.calc_h(newNode2)
+            if A_star.cycleCheck(newNode2):
+                returnValue.append(newNode2)
+
             newNode3 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode3.node[0][1], newNode3.node[1][1] = newNode3.node[1][1], newNode3.node[0][1]
             Node.calc_h(newNode3)
-            return [newNode1, newNode2, newNode3]
+            if A_star.cycleCheck(newNode3):
+                returnValue.append(newNode3)
+
+            return returnValue
 
         if positionZero[0] == 0 and positionZero[1] == 2:
             newNode1 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode1.node[0][2], newNode1.node[0][1] = newNode1.node[0][1], newNode1.node[0][2]
             Node.calc_h(newNode1)
+            if A_star.cycleCheck(newNode1):
+                returnValue.append(newNode1)
+
             newNode2 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode2.node[0][2], newNode2.node[1][2] = newNode2.node[1][2], newNode2.node[0][2]
             Node.calc_h(newNode2)
-            return [newNode1, newNode2]
+            if A_star.cycleCheck(newNode2):
+                returnValue.append(newNode2)
+
+            return returnValue
 
         if positionZero[0] == 1 and positionZero[1] == 0:
             newNode1 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode1.node[1][0], newNode1.node[0][0] = newNode1.node[0][0], newNode1.node[1][0]
             Node.calc_h(newNode1)
+            if A_star.cycleCheck(newNode1):
+                returnValue.append(newNode1)
+
             newNode2 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode2.node[1][0], newNode2.node[1][1] = newNode2.node[1][1], newNode2.node[1][0]
             Node.calc_h(newNode2)
+            if A_star.cycleCheck(newNode2):
+                returnValue.append(newNode2)
+
             newNode3 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode3.node[1][0], newNode3.node[2][0] = newNode3.node[2][0], newNode3.node[1][0]
             Node.calc_h(newNode3)
-            return [newNode1, newNode2, newNode3]
+            if A_star.cycleCheck(newNode3):
+                returnValue.append(newNode3)
+
+            return returnValue
 
         if positionZero[0] == 1 and positionZero[1] == 1:
             newNode1 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode1.node[1][1], newNode1.node[0][1] = newNode1.node[0][1], newNode1.node[1][1]
             Node.calc_h(newNode1)
+            if A_star.cycleCheck(newNode1):
+                returnValue.append(newNode1)
+
             newNode2 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode2.node[1][1], newNode2.node[1][0] = newNode2.node[1][0], newNode2.node[1][1]
             Node.calc_h(newNode2)
+            if A_star.cycleCheck(newNode2):
+                returnValue.append(newNode2)
+
             newNode3 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode3.node[1][1], newNode3.node[1][2] = newNode3.node[1][2], newNode3.node[1][1]
             Node.calc_h(newNode3)
+            if A_star.cycleCheck(newNode3):
+                returnValue.append(newNode3)
+
             newNode4 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode4.node[1][1], newNode4.node[2][1] = newNode4.node[2][1], newNode4.node[1][1]
             Node.calc_h(newNode4)
-            return [newNode1, newNode2, newNode3, newNode4]
+            if A_star.cycleCheck(newNode4):
+                returnValue.append(newNode4)
+
+            return returnValue
 
         if positionZero[0] == 1 and positionZero[1] == 2:
             newNode1 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode1.node[1][2], newNode1.node[0][2] = newNode1.node[0][2], newNode1.node[1][2]
             Node.calc_h(newNode1)
+            if A_star.cycleCheck(newNode1):
+                returnValue.append(newNode1)
+
             newNode2 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode2.node[1][2], newNode2.node[1][1] = newNode2.node[1][1], newNode2.node[1][2]
             Node.calc_h(newNode2)
+            if A_star.cycleCheck(newNode2):
+                returnValue.append(newNode2)
             newNode3 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode3.node[1][2], newNode3.node[2][2] = newNode3.node[2][2], newNode3.node[1][2]
             Node.calc_h(newNode3)
-            return [newNode1, newNode2, newNode3]
+            if A_star.cycleCheck(newNode3):
+                returnValue.append(newNode3)
+
+            return returnValue
 
         if positionZero[0] == 2 and positionZero[1] == 0:
             newNode1 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode1.node[2][0], newNode1.node[1][0] = newNode1.node[1][0], newNode1.node[2][0]
             Node.calc_h(newNode1)
+            if A_star.cycleCheck(newNode1):
+                returnValue.append(newNode1)
+
             newNode2 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode2.node[2][0], newNode2.node[2][1] = newNode2.node[2][1], newNode2.node[2][0]
             Node.calc_h(newNode2)
-            return [newNode1, newNode2]
+            if A_star.cycleCheck(newNode2):
+                returnValue.append(newNode2)
+
+            return returnValue
 
         if positionZero[0] == 2 and positionZero[1] == 1:
             newNode1 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode1.node[2][1], newNode1.node[2][0] = newNode1.node[2][0], newNode1.node[2][1]
             Node.calc_h(newNode1)
+            if A_star.cycleCheck(newNode1):
+                returnValue.append(newNode1)
+
             newNode2 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode2.node[2][1], newNode2.node[1][1] = newNode2.node[1][1], newNode2.node[2][1]
             Node.calc_h(newNode2)
+            if A_star.cycleCheck(newNode2):
+                returnValue.append(newNode2)
+
             newNode3 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode3.node[2][1], newNode3.node[2][2] = newNode3.node[2][2], newNode3.node[2][1]
             Node.calc_h(newNode3)
-            return [newNode1, newNode2, newNode3]
+            if A_star.cycleCheck(newNode3):
+                returnValue.append(newNode3)
+
+            return returnValue
 
         if positionZero[0] == 2 and positionZero[1] == 2:
             newNode1 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode1.node[2][2], newNode1.node[2][1] = newNode1.node[2][1], newNode1.node[2][2]
             Node.calc_h(newNode1)
+            if A_star.cycleCheck(newNode1):
+                returnValue.append(newNode1)
+
             newNode2 = Node(copy.deepcopy(currentNode.node), currentNode, currentNode.g+1)
             newNode2.node[2][2], newNode2.node[1][2] = newNode2.node[1][2], newNode2.node[2][2]
             Node.calc_h(newNode2)
-            return [newNode1, newNode2]
+            if A_star.cycleCheck(newNode2):
+                returnValue.append(newNode2)
+
+            return returnValue
